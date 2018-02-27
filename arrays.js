e6543b0fd17cea573f3beb33730064659f26ba6c
JulieSousa Sousa <juliesousa@yahoo.com>
2018-02-27 16:29:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,6 +16,6 @@ function destructivelyAddElementToEndOfArray(array, element){
   array.push(element)
   return array
 }
-function accessElementInArray() {
+function accessElementInArray(array, index) {
   
 }
